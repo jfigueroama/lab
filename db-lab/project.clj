a323651e66a44f453459a36c44520d17bde2b1b5
Jose Figueroa Martinez <coloso@gmail.com>
2016-12-12 09:27:15 -0600

    Jugando con gorilla-repl y bases de datos.

--- db-lab/project.clj
+++ db-lab/project.clj
@@ -6,4 +6,11 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.layerware/hugsql "0.4.7"]
                  [mysql/mysql-connector-java "6.0.4"]
-                 [com.hypirion/clj-xchart "0.2.0"]])
+                 [com.hypirion/clj-xchart "0.2.0"]
+                 [cljsjs/vega "2.6.0-0"]
+                 [cljsjs/d3 "4.3.0-2"]
+                 [gorilla-renderable "2.0.0"]
+                 [aysylu/loom "0.6.0"]
+                 [loom-gorilla "0.1.0"]
+                 ]
+  :plugins [[lein-gorilla "0.3.6"]])
--- db-lab/t.clj
+++ db-lab/t.clj
@@ -1,5 +1,7 @@
 (require '[hugsql.core :refer [db-run]])
 (require '[com.hypirion.clj-xchart :as c])
+#_(require '[gorilla-plot.core :as gp :refer [plot histogram bar-chart list-plot]])
+#_(require '[gorilla-repl.table :as gt :refer [table-view]])
 
 (def db {:subname (str "//localhost:3306/ca?"
                        "useUnicode=yes"
@@ -11,7 +13,7 @@
          :characterEncoding "UTF-8"
          :classname "com.mysql.cj.jdbc.Driver"
          :subprotocol "mysql"})
-         
+
 
 (db-run db "select * from profesor")
 
@@ -34,6 +36,13 @@
    (insert db sql {})))
 
 
+(exec db "CREATE TEMPORARY TABLE pjose AS (SELECT * FROM profesor WHERE nombres LIKE :nombres ESCAPE \"!\")" {:nombres "%os%"})
+
+(exec db "DROP VIEW pjose")
+
+
+(db-run db "CREATE TEMPORARY TABLE pjose AS (SELECT * FROM profesor WHERE nombres LIKE :nombres ESCAPE \"!\"); SELECT * FROM pjose " {:nombres "%os%"})
+
 
 (def profesxi
   (db-run db "
@@ -45,5 +54,25 @@
 (c/view
     (c/pie-chart (zipmap (map :instituto profesxi) (map :profes profesxi))
                  {:title "Profes x instituto"}))
+
+
+#_(let [profes (db-run db "select * from profesor")]
+      rows (map (fn [p]
+                  (map (fn [[k v]]v)
+                       p)) profes)
+      columns (mapv (fn [[k v]] k) (first profes)))
+  (table-view rows :columns columns)
+
+#_(let [r (db-run db "select i.id, i.nombre AS instituto, count(p.id) AS profesores FROM instituto AS i LEFT JOIN profesor AS p ON i.id=p.instituto_id GROUP BY i.id")])
+  (bar-chart (map :instituto r) (map :profesores r) :plot-size 2000)
+
+(defmacro qs
+  [db & args]
+  (let [se (str args)]
+  `(q db ~se)))
+
+(macroexpand-1 '(qs SELECT * FROM casa LIKE "\"mundo"))
+
+(qs db SELECT * FROM  profesor WHERE apellidos LIKE "%Figueroa%")
 
 
